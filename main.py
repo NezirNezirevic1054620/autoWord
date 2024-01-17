04d8fb80a663bdc6a3850a2bf03858f0649414b5
nezirnezirevic310 <1054620@hr.nl>
2024-01-17 21:36:49 +0100

    added welcome message

--- main.py
+++ main.py
@@ -6,48 +6,76 @@ from examples import custom_style_2
 from classes.cv_generator import CVGenerator
 from classes.essay_generator import EssayGenerator
 
-questions = [
+welcome_prompt = [
     {
-        "type": "list",
-        "name": "word",
-        "message": "What type of Word document would you like to generate?",
-        "choices": ["CV", "Essay"],
+        'type': 'confirm',
+        'message': 'Welcome to auto word generator! Would you like to proceed?',
+        'name': 'continue',
+        'default': True,
     }
 ]
 
-answers = prompt(questions, style=custom_style_2)
+welcome_prompt_answers = prompt(welcome_prompt, style=custom_style_2)
 
-if questions[0]["choices"][1]:
-    questions2 = [
+if list(welcome_prompt_answers.values())[0] is True:
+    questions = [
         {
-            "type": "input",
-            "name": "title",
-            "message": "What is the name of your essay?",
-        },
-        {
-            "type": "input",
-            "name": "paragraph",
-            "message": "Write your paragraph",
-        },
+            "type": "list",
+            "name": "word",
+            "message": "What type of Word document would you like to generate?",
+            "choices": ["Essay", "CV"],
+        }
     ]
 
-    answers2 = prompt(questions2, style=custom_style_2)
-    essay = EssayGenerator(title=list(answers2.values())[0], paragraph=list(answers2.values())[1])
-    essay.generate_essay()
+    answers = prompt(questions, style=custom_style_2)
 
-elif questions[0]["choices"][0]:
-    questions3 = [
-        {
-            "type": "input",
-            "name": "title",
-            "message": "What is the name of your CV",
-        },
-        {
-            "type": "input",
-            "name": "introduction",
-            "message": "Write your introduction",
-        },
-    ]
+    if questions[0]["choices"][0]:
+        questions2 = [
+            {
+                "type": "input",
+                "name": "title",
+                "message": "What is the name of your essay?",
+            },
+            {
+                "type": "input",
+                "name": "paragraph",
+                "message": "Write your paragraph",
+            },
+            {
+                'type': 'confirm',
+                'message': 'Do you want to continue?',
+                'name': 'continue',
+                'default': True,
+            }
+        ]
 
-    answers3 = prompt(questions3, style=custom_style_2)
-    cv = CVGenerator(title=list(answers3.values())[0], paragraph=list(answers3.values())[1])
+        answers2 = prompt(questions2, style=custom_style_2)
+
+        if list(answers2.values())[2] is True:
+            essay = EssayGenerator(title=list(answers2.values())[0], paragraph=list(answers2.values())[1])
+            essay.generate_essay()
+
+    elif questions[0]["choices"][1]:
+        questions3 = [
+            {
+                "type": "input",
+                "name": "title",
+                "message": "What is the name of your CV",
+            },
+            {
+                "type": "input",
+                "name": "introduction",
+                "message": "Write your introduction",
+            },
+            {
+                'type': 'confirm',
+                'message': 'Do you want to continue?',
+                'name': 'continue',
+                'default': True,
+            },
+        ]
+
+        answers3 = prompt(questions3, style=custom_style_2)
+        if list(answers3.values())[2] is True:
+            cv = CVGenerator(title=list(answers3.values())[0], paragraph=list(answers3.values())[1])
+            cv.generate_cv()
